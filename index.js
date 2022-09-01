ddae9f63944d3783a3f60982708c4ae8d42fa2b2
antieatingactivist <antieatingactivist@gmail.com>
2022-08-31 22:09:49 -0700

    serves static directory

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // Dependencies
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = 8000;
 
-
+app.use('/RGB-strip-controller', express.static(path.join(__dirname, "../build")));
 // Routes
-app.get('/', (req, res) => {
-  res.send("Henlo World");
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, "../build/index.html"));
+// });
+
+
 
 
 
