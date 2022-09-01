fe8ad469252eb64f20496c44bce090fd2f4c843d
antieatingactivist <antieatingactivist@gmail.com>
2022-09-01 15:09:19 -0700

    enables https

--- index.mjs
+++ index.mjs
@@ -2,6 +2,8 @@
 import express from 'express';
 import path from 'path';
 import {fileURLToPath} from 'url';
+import https from 'https';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,14 +14,27 @@ const PORT = 8000;
 
 app.use('/RGB-strip-controller', express.static(path.join(__dirname, "../build")));
 // Routes
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, "../build/index.html"));
-// });
-
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 
 
 
 // Listener
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+https
+  .createServer(
+    {
+        key: fs.readFileSync("key.pem"),
+        cert: fs.readFileSync("cert.pem"),
+    },
+
+    app
+    
+    
+    )
+  .listen(PORT, ()=>{
+    console.log(`App listening on PORT ${PORT}`)
+  });
+
+// app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
