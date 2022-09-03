5c3adcbfc5b1c26e4f1492a2cc6239b14de09ef5
antieatingactivist <antieatingactivist@gmail.com>
2022-09-03 00:39:45 -0700

    server test

--- index.mjs
+++ index.mjs
@@ -5,6 +5,13 @@ import https from 'https';
 import http from 'http';
 import fs from 'fs';
 import * as dotenv from 'dotenv';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+// import proxy from 'express-http-proxy';
+// import httpProxy from 'http-proxy';
+// var proxy = httpProxy.createProxyServer({target:'https://localhost:3001', secure: false}); 
+
+
+
 
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
@@ -15,7 +22,31 @@ const httpApp = express();
 const PORT = 443;
 
 
-app.enable('trust proxy');
+var options = {
+  target: 'http://test.com',
+  changeOrigin: true,
+  pathRewrite: {'^/api' : ''}      // <-- this will remove the /api prefix
+};
+
+
+
+
+app.use('/Tech-Blog/', createProxyMiddleware({ 
+  target: 'https://localhost:3001', 
+  changeOrigin: true, 
+  secure: false, 
+  pathRewrite: {'^/Tech-Blog' : ''} 
+}));
+
+// app.use(proxy('localhost:3001', {
+//   proxyReqPathResolver: function (req) {
+//     var parts = req.url.split('?');
+//     var queryString = parts[1];
+//     var updatedPath = parts[0].replace(/Tech-Blog/, '');
+//     return updatedPath + (queryString ? '?' + queryString : '');
+//   }
+// }));
+
 
 // app.use('/RGB-strip-controller', express.static(path.join(__dirname, "../build")));
 
@@ -42,9 +73,10 @@ fs.readdir(__dirname+"/public", (err, files) => {
           path: path.join("./public/" + project + "/.env")
         }).parsed.PORT;
         console.log(port);
-        app.get("/" + project, function(req, res, next) {
-          res.redirect(`https://localhost:${port}/`);
-        });
+        // app.get("/" + project, function(req, res, next) {
+        //   res.redirect(`https://localhost:${port}/`);
+
+        // });
       }
     });
 
@@ -100,7 +132,7 @@ https.createServer({
 },app)
   .listen(PORT, ()=>{
     console.log(`App listening on PORT ${PORT}`);
-    console.log(process.env);
+
   });
 //redirect server
 http.createServer(httpApp)
